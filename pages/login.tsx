4d046709e8a967aebd4d7c536f961d896b606e47
manlounge <knowledgemaxlevel@gmail.com>
2025-05-17 21:36:35 +1000

    Create login.tsx

---
+++ pages/login.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h2>로그인 페이지 준비 중…</h2>
+      <p>곧 로그인 기능이 추가될 예정입니다.</p>
+    </div>
+  );
+}
