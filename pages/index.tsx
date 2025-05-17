b8cd1b7a19a2581324cd6443bc213c0f267ce60c
manlounge <knowledgemaxlevel@gmail.com>
2025-05-17 21:36:22 +1000

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>🎉 맨라운지에 오신 걸 환영합니다</h1>
+      <p>지금은 베타 테스트 단계입니다. 피드백 부탁드려요!</p>
+    </div>
+  );
+}
