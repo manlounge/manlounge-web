8f0dc5423bcb60e5234055bf0bc0890740ed8988
manlounge <knowledgemaxlevel@gmail.com>
2025-05-18 16:12:06 +1000

    Delete pages/auth directory

--- pages/auth/kakao.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/kakao/callback`;
-  const kakaoKey = process.env.KAKAO_REST_API_KEY;
-  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoKey}&redirect_uri=${redirectUri}&response_type=code`;
-  return res.redirect(url);
-}
--- pages/auth/naver.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const state = Math.random().toString(36).substring(2, 15);
-  const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/naver/callback`;
-  const naverKey = process.env.NAVER_CLIENT_ID;      // .env.local 에 등록한 키
-  const url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverKey}&redirect_uri=${redirectUri}&state=${state}`;
-  return res.redirect(url);
-}
